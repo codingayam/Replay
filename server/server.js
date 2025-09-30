e2842c0767f217674258d546c7f627cb8f077533
xj <ngxujie1993@gmail.com>
2025-09-30 15:51:00 +0800

    onesignal fixes by claude

--- server/server.js
+++ server/server.js
@@ -636,7 +636,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-OneSignal-Subscription-Id']
 }));
 
 // Body parsing middleware
