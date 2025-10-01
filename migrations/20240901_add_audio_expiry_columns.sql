924cb02e59381398306980d8d88471d89b0f87f2
xj <ngxujie1993@gmail.com>
2025-10-01 15:05:02 +0800

    Changed app icons

--- migrations/20240901_add_audio_expiry_columns.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE IF EXISTS meditations
-  ADD COLUMN IF NOT EXISTS audio_storage_path text,
-  ADD COLUMN IF NOT EXISTS audio_expires_at timestamptz,
-  ADD COLUMN IF NOT EXISTS audio_removed_at timestamptz;
